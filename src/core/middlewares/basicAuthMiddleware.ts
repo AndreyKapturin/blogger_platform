c117ddd93f5535ae1d6875e8144efc7941207e6c
Andrey Kapturin <andruxa21097@gmail.com>
2026-01-14 15:42:17 +0700

    feat: Basic auth middleware

---
+++ src/core/middlewares/basicAuthMiddleware.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpStatus } from "../types/HttpStatus";
+
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'qwerty';
+
+const basicAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.sendStatus(HttpStatus.Unauthorized);
+    return;
+  }
+  const [ authType, credentials ] = authHeader.split(' ');
+
+  if (authType !== 'Basic') {
+    res.sendStatus(HttpStatus.Unauthorized);
+    return;
+  }
+
+  if (!credentials || credentials.length === 0) {
+    res.sendStatus(HttpStatus.Unauthorized);
+    return;
+  }
+
+  const decodedCredentials = Buffer.from(credentials, 'base64').toString('utf-8');
+
+  const [ login, password ] = decodedCredentials.split(':');
+
+   if (login !== ADMIN_LOGIN || password !== ADMIN_PASSWORD) {
+    res.sendStatus(HttpStatus.Unauthorized);
+    return;
+  }
+
+  next()
+}
+
+export { basicAuthMiddleware }
--- src/entities/blogs/router.ts
+++ src/entities/blogs/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { blogsController } from './controller';
 import { validationResultMiddleware } from '../../core/validation/validationMiddleware';
 import { inputBlogValidationSchema } from './validations';
+import { basicAuthMiddleware } from '../../core/middlewares/basicAuthMiddleware';
 
 const blogsRouter = Router();
 
@@ -9,15 +10,18 @@ blogsRouter.get('/', blogsController.getBlogs);
 blogsRouter.get('/:id', blogsController.getBlogById);
 blogsRouter.post(
   '/',
+  basicAuthMiddleware,
   inputBlogValidationSchema,
   validationResultMiddleware,
   blogsController.createBlog
 );
-blogsRouter.put('/:id',
+blogsRouter.put(
+  '/:id',
+  basicAuthMiddleware,
   inputBlogValidationSchema,
   validationResultMiddleware,
   blogsController.updateBlog
 );
-blogsRouter.delete('/:id', blogsController.deleteBlog);
+blogsRouter.delete('/:id', basicAuthMiddleware, blogsController.deleteBlog);
 
 export { blogsRouter };
