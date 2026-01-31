a5b206e47684a06e4f1877ec5ba52359d4a0f434
Andrey Kapturin <andruxa21097@gmail.com>
2026-01-31 17:30:48 +0700

    tests: Add tests for create user endpoint

--- __tests__/e2e/entities/users/create.spec.ts
+++ __tests__/e2e/entities/users/create.spec.ts
@@ -10,6 +10,12 @@ import { authHeader } from '../../../../src/core/constants';
 
 let app: Express;
 
+const inputUser: InputUserType = {
+  login: 'AndrewK',
+  email: 'andrew@mail.ru',
+  password: 'Qwerty12345!',
+};
+
 beforeAll(async () => {
   app = await createApp();
 });
@@ -20,12 +26,6 @@ beforeEach(async () => {
 
 describe(`POST ${Routes.Users}`, () => {
   it('should create user if all data is correct', async () => {
-    const inputUser: InputUserType = {
-      login: 'AndrewK',
-      email: 'andrew@mail.ru',
-      password: 'Qwerty12345!',
-    };
-
     const response = await request(app)
       .post(Routes.Users)
       .set('Authorization', authHeader)
@@ -38,6 +38,39 @@ describe(`POST ${Routes.Users}`, () => {
       login: inputUser.login,
       email: inputUser.email,
     });
+  });
+
+  it(`should return ${HttpStatus.Bad_Request} if input data has incorrect fields`, async () => {
+    const incorrectInputUser = {
+      login: 'A',
+      email: 'andrew@@@mail.ru',
+    };
+    const response = await request(app)
+      .post(Routes.Users)
+      .set('Authorization', authHeader)
+      .send(incorrectInputUser);
+    expect(response.status).toBe(HttpStatus.Bad_Request);
+    expect(response.body).toEqual({
+      errorsMessages: expect.arrayContaining([
+        {
+          field: 'password',
+          message: expect.any(String),
+        },
+        {
+          field: 'login',
+          message: expect.any(String),
+        },
+        {
+          field: 'email',
+          message: expect.any(String),
+        },
+      ]),
+    });
+  });
+
+  it(`should return ${HttpStatus.Unauthorized} if auth header not passed`, async () => {
+    const response = await request(app).post(Routes.Users).send(inputUser);
+    expect(response.status).toBe(HttpStatus.Unauthorized);
   });
 });
 
